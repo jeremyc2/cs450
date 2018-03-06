9b832e0b99ebc6ef5f6de297878fc496ad811caa
jeremyc2 <goldenmeso@gmail.com>
2018-03-05 21:01:54 -0700

    Add files via upload

--- SVM.R
+++ SVM.R
@@ -31,6 +31,8 @@ runLetters <- function(g, c){
   testRows <- sample(allRows, trunc(length(allRows) * 0.3))
   lettersTest <- letters[testRows,]
   lettersTrain <- letters[-testRows,]
+  lettersTrain <- sample(1:nrow(lettersTrain), trunc(length(1:nrow(lettersTrain)) * 0.01))
+  lettersTrain <- letters[lettersTrain,]
   model <- svm(letter~., data = lettersTrain, kernel = "radial", gamma = g, cost = c)
   
   prediction <- predict(model, lettersTest[,-1])
@@ -43,12 +45,12 @@ runLetters <- function(g, c){
 }
 
 vowelMatrix <- function(gamma){
-  invisible(mapply(runVowel, g = gamma , c = 2^(seq(-15,5,2))))
+  invisible(mapply(runVowel, g = gamma , c = 2^(seq(-15,15,2))))
 }
 
 letterMatrix <- function(gamma){
-  invisible(mapply(runLetters, g = gamma , c = 2^(seq(-15,5,2))))
+  invisible(mapply(runLetters, g = gamma , c = 2^(seq(-15,15,2))))
 }
 
-mapply(vowelMatrix, g = 2^(seq(-5,15,2)))
-mapply(letterMatrix, g = 2^(seq(-5,15,2)))
+mapply(vowelMatrix, g = 2^(seq(-15,15,2)))
+mapply(letterMatrix, g = 2^(seq(-15,15,2)))
